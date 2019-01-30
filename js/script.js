3bbfb4823e77d65605614e34104e4ffccc9dab0c
Hungry-Silence <__08>
2019-01-31 00:18:30 +0300

    sign-in/out

--- js/script.js
+++ js/script.js
@@ -1,9 +1,37 @@
 var hamburger_menu = function() {
-    var hamburger = document.querySelector(".fas");
     var menu = document.querySelector(".hamburger-nav");
     if (menu.classList.contains("hidden")) {
-            menu.classList.remove("hidden");   
+        menu.classList.remove("hidden");   
     } else {
-            menu.classList.add("hidden");
+        menu.classList.add("hidden");
     }
+};
+
+var sign_up = document.querySelector(".sign-up_window"); 
+
+var sign_up_open = function() {
+    sign_up.classList.remove("hidden");
+};
+
+var sign_up_close = function() {
+    var sign_up_button = document.querySelector(".sign-up_button").textContent = "Войти";
+    var d = document.querySelector(".sign-in a").textContent = "Ещё не зарегистрированы?";
+    var mail = document.querySelector("#e-mail");
+    var confirm_password = document.querySelector("#confirm_password");
+    mail.classList.add("hidden");
+    confirm_password.classList.add("hidden");
+    sign_up.classList.add("hidden");
+};
+
+var sign_in_show = function() {
+    var sign_in = document.querySelectorAll(".sign-up_create_member");
+    var sign_up_button = document.querySelector(".sign-up_button").textContent = "Зарегистрироваться!";
+    var d = document.querySelector(".sign-in a").textContent = "Уже есть аккаунт?";
+    
+    for (var i = 0; i < sign_in.length; ++i) {
+    var member = sign_in[i];
+        if (member.classList.contains("hidden")) {
+            member.classList.remove("hidden");
+        };
+    };
 };
